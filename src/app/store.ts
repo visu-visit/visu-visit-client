47d7fc4ecc7b293d376ce61a345adc50326d859a
suhyeonglee <sounmind777@gmail.com>
2021-10-01 03:28:09 +0900

    feat(*): initial setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import historyReducer from "../features/history/historySlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    history: historyReducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
