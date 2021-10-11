bca4d900c936b555621eaac872a94936b9148dde
suhyeonglee <sounmind777@gmail.com>
2021-10-12 01:24:52 +0900

    feat(DirectedGraph): save dragged node position

--- src/components/DirectedGraph.tsx
+++ src/components/DirectedGraph.tsx
@@ -1,22 +1,9 @@
 /* eslint-disable no-param-reassign */
 
-import {
-  MouseEvent,
-  MouseEventHandler,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import { useSelector } from "react-redux";
+import { MouseEvent, useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import {
-  forceManyBody,
-  forceLink,
-  forceSimulation,
-  forceX,
-  forceY,
-  forceCenter,
-} from "d3-force";
+import { forceManyBody, forceLink, forceSimulation, forceX, forceY } from "d3-force";
 import { schemeCategory10 } from "d3-scale-chromatic";
 import { scaleOrdinal } from "d3-scale";
 import { select } from "d3-selection";
@@ -24,15 +11,23 @@ import { zoom } from "d3-zoom";
 import { drag } from "d3-drag";
 
 import { RootState } from "../app/store";
-import { IDomainLink, IDomainNode, IVisit } from "../types/history";
-import { URL_TRANSITION_TYPES } from "../constants/history";
+import { IClickedNode, IDomainLink, IDomainNode, IVisit } from "../types/history";
+import { DEFAULT_STROKE_WIDTH, URL_TRANSITION_TYPES } from "../constants/history";
 import Modal from "./shared/Modal";
 import NodeDetail from "./NodeDetail";
 import {
   convertToRadius,
+  getArrowPosition,
+  handleNodeRadius,
+  handleLinkDistance,
+  handleLinkStroke,
+  handleMouseOut,
+  handleMouseOver,
+  handleNodeStrength,
   makeGraphData,
   removeSpecialCases,
 } from "../util/history";
+import { changeNode } from "../features/history/historySlice";
 
 const Wrapper = styled.div`
   margin: 20px;
@@ -53,53 +48,51 @@ const Board = styled.svg`
 `;
 
 const color = scaleOrdinal(URL_TRANSITION_TYPES, schemeCategory10);
-const FORCE_STRENGTH = -800;
-const DEFAULT_IMAGE_SIZE = {
-  width: 20,
-  height: 20,
-};
+const INITIAL_CLICKED_NODE = { top: 0, left: 0, node: {} as IDomainNode };
 
 export default function DirectedGraph() {
-  const totalVisits = useSelector<RootState, IVisit[]>(
-    ({ history }) => history.totalVisits,
-  );
-  const domainNodes = useSelector<RootState, IDomainNode[]>(
-    ({ history }) => history.domainNodes,
-  );
+  const totalVisits = useSelector<RootState, IVisit[]>(({ history }) => history.totalVisits);
+  const domainNodes = useSelector<RootState, IDomainNode[]>(({ history }) => history.domainNodes);
   const svgRef = useRef<SVGSVGElement>(null);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [clickedPosition, setClickedPosition] = useState({ top: 0, left: 0 });
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [clickedNode, setClickedNode] = useState<IClickedNode>(INITIAL_CLICKED_NODE);
+  const [zoomTransform, setZoomTransform] = useState<any>(null);
+  const dispatch = useDispatch();
 
-  const handleCloseModal: MouseEventHandler<HTMLButtonElement> = () => {
+  const handleCloseModal: () => void = () => {
     setIsModalVisible(false);
   };
 
   useEffect(() => {
-    const clientRect = svgRef.current?.getBoundingClientRect();
-
-    if (clientRect === undefined) {
+    if (!svgRef.current) {
       return;
     }
 
-    const { width, height } = clientRect;
+    const { width, height } = svgRef.current.getBoundingClientRect();
     const { nodes, links } = makeGraphData(totalVisits, domainNodes);
-
     const svg = select<SVGSVGElement, unknown>(svgRef.current as SVGSVGElement);
 
     svg.selectAll("*").remove();
 
     const container = svg.append("g");
 
+    if (zoomTransform) {
+      container.attr(
+        "transform",
+        `translate(${zoomTransform.x},${zoomTransform.y}) scale(${zoomTransform.k})`,
+      );
+    }
+
     const handleZoom = (event: any) => {
       container.attr("transform", event.transform);
+      setZoomTransform(event.transform);
     };
-
     const zoomBehavior = zoom<SVGSVGElement, unknown>()
       .extent([
         [0, 0],
         [width, height],
       ])
-      .scaleExtent([-4, 4])
+      .scaleExtent([0, 2])
       .on("zoom", handleZoom);
 
     svg.call(zoomBehavior).on("dblclick.zoom", null);
@@ -107,19 +100,32 @@ export default function DirectedGraph() {
     const simulation = forceSimulation<IDomainNode, IDomainLink>(nodes)
       .force(
         "link",
-        forceLink<IDomainNode, IDomainLink>(links).id(({ name }) => name),
+        forceLink<IDomainNode, IDomainLink>(links)
+          .distance(handleLinkDistance)
+          .id(({ name }) => name),
       )
-      .force("center", forceCenter(width / 2, height / 2))
-      .force("charge", forceManyBody().strength(FORCE_STRENGTH))
+      .force("charge", forceManyBody<IDomainNode>().strength(handleNodeStrength))
       .force("x", forceX(width / 2))
       .force("y", forceY(height / 2));
+
+    const handleDrag = ({ x, y }: DragEvent, node: IDomainNode) => {
+      node.fx = x;
+      node.fy = y;
+      simulation.alpha(1).restart();
+    };
+    const handleDragEnd = ({ x, y }: DragEvent, node: IDomainNode) => {
+      dispatch(changeNode(node));
+    };
+    const dragBehavior = drag<SVGSVGElement, IDomainNode>()
+      .on("drag", handleDrag)
+      .on("end", handleDragEnd);
 
     container
       .append("svg:defs")
       .selectAll("marker")
       .data(URL_TRANSITION_TYPES)
       .join("marker")
-      .attr("id", (datum) => `arrow-${datum}`)
+      .attr("id", (urlTransition) => `arrow-${urlTransition}`)
       .attr("viewBox", "0 -5 10 10")
       .attr("refX", 10)
       .attr("refY", -1.5)
@@ -136,9 +142,9 @@ export default function DirectedGraph() {
       .selectAll("path")
       .data<IDomainLink>(links)
       .join("path")
-      .attr("stroke-width", 1.5)
-      .attr("stroke", (datum) => color(datum.type))
-      .attr("marker-end", (datum) => `url(#arrow-${datum.type})`);
+      .attr("stroke-width", handleLinkStroke)
+      .attr("stroke", (domainLink) => color(domainLink.type))
+      .attr("marker-end", (domainLink) => `url(#arrow-${domainLink.type})`);
 
     const nodeGroup = container
       .append<SVGSVGElement>("svg:g")
@@ -146,87 +152,60 @@ export default function DirectedGraph() {
       .data<IDomainNode>(nodes)
       .join("g")
       .attr("class", "history-node")
-      .attr("id", (datum) => removeSpecialCases(datum.name));
+      .attr("id", (domainNode) => removeSpecialCases(domainNode.name));
 
     nodeGroup
       .append("circle")
-      .attr("fill", "white")
+      .attr("fill", ({ color: nodeColor }) => nodeColor || "white")
       .attr("stroke", "gray")
-      .attr("stroke-width", 1.5)
-      .attr("r", (datum) => convertToRadius(datum.visitCount));
+      .attr("stroke-width", DEFAULT_STROKE_WIDTH)
+      .attr("r", handleNodeRadius)
+      .join("circle");
 
     nodeGroup
       .append("image")
-      .attr(
-        "xlink:href",
-        (datum) =>
-          `https://www.google.com/s2/favicons?sz=32&domain=${datum.name}`,
-      )
+      .attr("xlink:href", ({ name }) => `https://api.faviconkit.com/${new URL(name).hostname}/128`)
       .on("error", ({ target }) => {
-        select(target).attr(
-          "xlink:href",
-          "/images/graph/defaultNodeFavicon.png",
-        );
+        select(target).attr("xlink:href", "/images/graph/defaultNodeFavicon.png");
       })
-      .attr("width", DEFAULT_IMAGE_SIZE.width)
-      .attr("height", DEFAULT_IMAGE_SIZE.height)
-      .attr("transform", "translate(-10 -10)");
+      .attr("width", (node) => handleNodeRadius(node))
+      .attr("height", (node) => handleNodeRadius(node))
+      .attr("x", (node) => -handleNodeRadius(node) / 2)
+      .attr("y", (node) => -handleNodeRadius(node) / 2);
 
-    const handleDrag = ({ x, y }: DragEvent, datum: IDomainNode) => {
-      datum.fx = x;
-      datum.fy = y;
-      simulation.alpha(1).restart();
-    };
+    nodeGroup
+      .append("circle")
+      .attr("fill", "red")
+      .attr("opacity", 0.5)
+      .attr("r", (node) => (node.memo ? handleNodeRadius(node) / 10 : 0))
+      .attr("width", 20)
+      .attr("height", 20)
+      .attr(
+        "transform",
+        (node) => `translate(${handleNodeRadius(node) / 2} -${handleNodeRadius(node) / 2})`,
+      );
 
-    const handleContextMenu = (event: MouseEvent, datum: IDomainNode) => {
+    const handleContextMenu = (event: MouseEvent, domainNode: IDomainNode) => {
       event.preventDefault();
 
-      if (!datum) {
-        return;
-      }
-
+      setClickedNode({
+        top: event.clientY,
+        left: event.clientX,
+        node: domainNode,
+      });
       setIsModalVisible(true);
-      setClickedPosition({ top: event.clientY, left: event.clientX });
     };
 
-    const handleDoubleClick = (event: MouseEvent, datum: IDomainNode) => {
-      if (!datum) {
+    const handleDoubleClick = (event: MouseEvent, domainNode: IDomainNode) => {
+      if (!domainNode) {
         return;
       }
 
-      delete datum.fx;
-      delete datum.fy;
+      delete domainNode.fx;
+      delete domainNode.fy;
 
       simulation.alpha(1).restart();
     };
-
-    const handleMouseOver = (event: MouseEvent, datum: IDomainNode) => {
-      if (!datum) {
-        return;
-      }
-
-      select(`#${removeSpecialCases(datum.name)}`)
-        .append("text")
-        .attr("x", 8)
-        .attr("y", "0.31em")
-        .text(datum.name.split("//")[1])
-        .attr("fill", "black")
-        .attr("font-size", "10px")
-        .attr("transform", "translate(10 0)");
-    };
-
-    const handleMouseOut = (event: MouseEvent, datum: IDomainNode) => {
-      if (!datum) {
-        return;
-      }
-
-      select(`#${removeSpecialCases(datum.name)} text`).remove();
-    };
-
-    const dragBehavior = drag<SVGSVGElement, IDomainNode>().on(
-      "drag",
-      handleDrag,
-    );
 
     container
       .selectAll<SVGSVGElement, IDomainNode>(".history-node")
@@ -236,58 +215,36 @@ export default function DirectedGraph() {
       .on("mouseover", handleMouseOver)
       .on("mouseout", handleMouseOut);
 
-    simulation.on("tick", () => {
-      linkGroup.attr("d", (datum) => {
+    const handleTick = () => {
+      linkGroup.attr("d", (domainLink) => {
+        const visitCount = nodes.find((node) => node.name === domainLink.target.name)?.visitCount;
         let radius = 20;
-        const visitCount = nodes.find(
-          (node) => node.name === datum.target.name,
-        )?.visitCount;
 
         if (visitCount) {
           radius = convertToRadius(visitCount);
         }
 
-        if (
-          datum.target.x === undefined ||
-          datum.target.y === undefined ||
-          datum.source.x === undefined ||
-          datum.source.y === undefined
-        ) {
-          return "M0, -5L10, 0L0, 5";
-        }
-
-        const dx = datum.target.x - datum.source.x;
-        const dy = datum.target.y - datum.source.y;
-        const dr = Math.sqrt(dx * dx + dy * dy);
-
-        const offsetX = (dx * radius) / dr;
-        const offsetY = (dy * radius) / dr;
-
-        const placeX = datum.target.x - offsetX;
-        const placeY = datum.target.y - offsetY;
-
-        const px = datum.source.x;
-        const py = datum.source.y;
-
-        return `M${px},${py}A${dr},${dr} 0 0,1 ${placeX},${placeY}`;
+        return getArrowPosition(domainLink, radius);
       });
 
-      nodeGroup.attr(
-        "transform",
-        (datum: any) => `translate(${datum.x},${datum.y})`,
-      );
-    });
-  }, [totalVisits]);
+      nodeGroup.attr("transform", (domainNode) => `translate(${domainNode.x},${domainNode.y})`);
+    };
+
+    simulation.on("tick", handleTick);
+  }, [totalVisits, domainNodes]);
 
   return (
     <Wrapper>
       <Board ref={svgRef} />
       {isModalVisible && (
         <Modal
-          position={{ top: clickedPosition.top, left: clickedPosition.left }}
+          position={{
+            top: clickedNode.top,
+            left: clickedNode.left,
+          }}
           handleClose={handleCloseModal}
         >
-          <NodeDetail />
+          <NodeDetail node={clickedNode.node} handleClose={handleCloseModal} />
         </Modal>
       )}
     </Wrapper>
