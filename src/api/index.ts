ba46045570f1712ba67fd7cd8d34b65fa5888e2c
suhyeonglee <sounmind777@gmail.com>
2021-10-12 01:28:41 +0900

    feat(History, api): fetch when component mounted

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,41 +1,37 @@
 import { nanoid } from "nanoid";
-import { IPostHistoryResponse, IHistoryFormData } from "../types/history";
+import { IHistoryApiResponse, IHistoryFormData } from "../types/history";
 
-export const postHistoryFile = async (
-  file: File,
-): Promise<IPostHistoryResponse> => {
+export const postHistoryFile = async (file: File): Promise<IHistoryApiResponse> => {
   const formData = new FormData();
 
   formData.append("historyFile", file, file.name);
 
-  const response = await fetch(
-    `${process.env.REACT_APP_SERVER_URL}/browser-history/${nanoid()}`,
-    {
-      method: "POST",
-      body: formData,
-    },
-  );
+  const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/browser-history/${nanoid()}`, {
+    method: "POST",
+    body: formData,
+  });
 
   return response.json();
 };
 
-export const getHistory = async ({
+export const fetchHistory = async ({
   id,
   formData,
 }: {
   id: string;
-  formData: IHistoryFormData;
-}): Promise<IPostHistoryResponse> => {
-  const { start, end, domain } = formData;
-  const url = new URL(
-    `${process.env.REACT_APP_SERVER_URL}/browser-history/${id}`,
-  );
+  formData?: IHistoryFormData;
+}): Promise<IHistoryApiResponse> => {
+  const url = new URL(`${process.env.REACT_APP_SERVER_URL}/browser-history/${id}`);
 
-  url.searchParams.append("start", start);
-  url.searchParams.append("end", end);
+  if (formData) {
+    const { start, end, domain } = formData;
 
-  if (domain) {
-    url.searchParams.append("domain", domain);
+    url.searchParams.append("start", start);
+    url.searchParams.append("end", end);
+
+    if (domain) {
+      url.searchParams.append("domain", domain);
+    }
   }
 
   const response = await fetch(url.toString());
