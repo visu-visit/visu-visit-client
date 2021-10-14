57aa02fe412c4b45f66b84a37ac163cd94b2ffa1
suhyeonglee <sounmind777@gmail.com>
2021-10-15 00:41:55 +0900

    feat(History, Introduction): add loading

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -8,33 +8,47 @@ import { updateBrowserHistory } from "../features/history/historySlice";
 import ErrorMessage from "../components/shared/ErrorMessage";
 import Title from "../components/shared/Title";
 import { postHistoryFile } from "../api";
+import Loading from "../components/shared/Loading";
+import flexCenter from "../styles/flexCenter";
 
 const Wrapper = styled.div`
+  ${flexCenter}
   position: relative;
-  display: flex;
   width: 100%;
   height: 100%;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
 `;
 
 const MainTitle = styled(Title)`
-  position: absolute;
-  top: 0;
-  left: 0;
+  font-size: 40px;
 `;
 
 const Section = styled.section`
   position: relative;
   width: 50%;
   min-width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 200px;
+  height: 200px;
 `;
 
 const SLink = styled(Link)`
   display: block;
   text-decoration: underline;
   margin: 10px 0 10px 0;
+  font-size: 20px;
+  padding: 10px;
+
+  &:hover {
+    border: 0px;
+    border-radius: 5px;
+    background-color: lightgray;
+  }
 `;
 
 const Label = styled.label`
@@ -43,12 +57,12 @@ const Label = styled.label`
   justify-content: center;
   font-family: Montserrat;
   font-weight: 200;
-  width: 100%;
-  height: 100px;
+  width: 200px;
+  height: 50px;
   background: ${({ theme }) => theme.gradient.orange};
   border-radius: 10px;
   box-shadow: ${({ theme }) => theme.boxShadow};
-  font-size: 50px;
+  font-size: 20px;
 
   &:hover {
     border: 1px solid gray;
@@ -63,10 +77,12 @@ const Input = styled.input`
 export default function Introduction() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleUploadFile = async () => {
+    setIsLoading(true);
     setErrorMessage("");
 
     const file = inputRef.current?.files?.[0];
@@ -89,26 +105,33 @@ export default function Introduction() {
         setErrorMessage("히스토리 파일 업로드에 실패했습니다.");
       }
     }
+
+    setIsLoading(false);
   };
 
   return (
     <Wrapper>
       <MainTitle>Visualize & Customize Your Browser History</MainTitle>
+      <Logo alt="logo" src="./images/logo.png" />
       <Section>
         <SLink to="description-about-browser-history-location">
           HOW & WHERE TO GET BROWSER HISTORY FILE ?
         </SLink>
 
-        <Label htmlFor="historyFileInput">
-          UPLOAD
-          <Input
-            id="historyFileInput"
-            ref={inputRef}
-            onChange={handleUploadFile}
-            type="file"
-            name="historyFile"
-          />
-        </Label>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Label htmlFor="historyFileInput">
+            UPLOAD
+            <Input
+              id="historyFileInput"
+              ref={inputRef}
+              onChange={handleUploadFile}
+              type="file"
+              name="historyFile"
+            />
+          </Label>
+        )}
 
         {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
       </Section>
